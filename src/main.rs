15b0e6f8878eb073a6f40f1e8dd42588d7b57624
ross <rostikvunarchuk@gmail.com>
2025-12-14 21:08:11 +0200

    few lings

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn triple(number: i32) -> i32 {
     return number * 3;
 }
 
+
 fn main() {
     println!("Hello, world!");
 
@@ -19,6 +20,107 @@ fn main() {
     let num = 2;
     let tripled = triple(num);
 
-    println!("{tripled}")
+    println!("{tripled}");
 
+    let mut counter = 0;
+    let some_number = loop {
+        counter += 1;
+        
+        if counter == 10 {
+            break counter;
+        }
+
+    };
+
+    println!("{some_number}");
+
+    println!("iterating over range");
+    for number in 1..10 {
+        println!("{number}");
+    }
+
+    println!("reverse order iterator");
+    for number in (1..10).rev() {
+        println!("{number}");
+    }
+
+}
+
+// TODO: Fix the compiler error on this function.
+fn picky_eater(food: &str) -> &str {
+    if food == "strawberry" {
+        "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else {
+        "No thanks!"
+    }
+}
+
+fn animal_habitat(animal: &str) -> &str {
+    // TODO: Fix the compiler error in the statement below.
+    let identifier = if animal == "crab" {
+        1
+    } else if animal == "gopher" {
+        2
+    } else if animal == "snake" {
+        3
+    } else {
+        4
+    };
+
+    // Don't change the expression below!
+    if identifier == 1 {
+        "Beach"
+    } else if identifier == 2 {
+        "Burrow"
+    } else if identifier == 3 {
+        "Desert"
+    } else {
+        "Unknown"
+    }
+}
+
+// Don't change the tests!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn yummy_food() {
+        // This means that calling `picky_eater` with the argument "strawberry" should return "Yummy!".
+        assert_eq!(picky_eater("strawberry"), "Yummy!");
+    }
+
+    #[test]
+    fn neutral_food() {
+        assert_eq!(picky_eater("potato"), "I guess I can eat that.");
+    }
+
+    #[test]
+    fn default_disliked_food() {
+        assert_eq!(picky_eater("broccoli"), "No thanks!");
+        assert_eq!(picky_eater("gummy bears"), "No thanks!");
+        assert_eq!(picky_eater("literally anything"), "No thanks!");
+    }
+
+    #[test]
+    fn gopher_lives_in_burrow() {
+        assert_eq!(animal_habitat("gopher"), "Burrow")
+    }
+
+    #[test]
+    fn snake_lives_in_desert() {
+        assert_eq!(animal_habitat("snake"), "Desert")
+    }
+
+    #[test]
+    fn crab_lives_on_beach() {
+        assert_eq!(animal_habitat("crab"), "Beach")
+    }
+
+    #[test]
+    fn unknown_animal() {
+        assert_eq!(animal_habitat("dinosaur"), "Unknown")
+    }
 }
